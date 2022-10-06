9452e876bc6c2517ab105030f95df7fb528a2b72
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-10-06 14:34:10 -0400

    create, configure, & connect an indexeddb database to app

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -3,15 +3,19 @@ import "./form.js";
 import "./submit.js";
 // Import CSS files
 import "../css/index.css";
-import { Tooltip, Toast, Popover } from 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Tooltip, Toast, Popover } from "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 // Import image files
-import Logo from '../images/logo.png';
-import Bear from '../images/bear.png';
-import Dog from '../images/dog.png';
+import Logo from "../images/logo.png";
+import Bear from "../images/bear.png";
+import Dog from "../images/dog.png";
 
-window.addEventListener('load', function () {
-    document.getElementById('logo').src = Logo;
-    document.getElementById('bearThumbnail').src = Bear;
-    document.getElementById('dogThumbnail').src = Dog;
-  });
+// Import IndexedDB
+import { initdb } from "./database";
+
+window.addEventListener("load", function () {
+  initdb();
+  document.getElementById("logo").src = Logo;
+  document.getElementById("bearThumbnail").src = Bear;
+  document.getElementById("dogThumbnail").src = Dog;
+});
