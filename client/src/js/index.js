b091a4ac3c7139669ed89a8b605f1096c426725c
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-10-06 15:07:52 -0400

    post & get data

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -11,11 +11,15 @@ import Bear from "../images/bear.png";
 import Dog from "../images/dog.png";
 
 // Import IndexedDB
-import { initdb } from "./database";
+import { initDb, getDb, postDb } from './database';
 
 window.addEventListener("load", function () {
-  initdb();
+  initDb();
   document.getElementById("logo").src = Logo;
   document.getElementById("bearThumbnail").src = Bear;
   document.getElementById("dogThumbnail").src = Dog;
+    // We are temporarily placing getDb() and postDb() function calls here for testing. We will move it to another event listener later.
+    getDb();
+    postDb("Lernantino", "learnantino@test.com", 8186601234, "Bear");
+    getDb();
 });
