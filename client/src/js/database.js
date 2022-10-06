94f1c4de6239c0d100ed3d94920d6221939e0e3c
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-10-06 16:00:57 -0400

    add update functionality

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -81,4 +81,20 @@ export const postDb = async (name, email, phone, profile) => {
     console.log('result.value', result);
     return result?.value;
   };
+
+  // EXPORTED EDIT function
+export const editDb = async (id, name, email, phone, profile) => {
+    console.log('PUT to the database');
+    // Create a connection to the IndexedDB database and the version we want to use.
+    const contactDb = await openDB('contact_db', 1);
+    // Create a new transaction and specify the database and data privileges.
+    const tx = contactDb.transaction('contacts', 'readwrite');
+    // Open up the desired object store.
+    const store = tx.objectStore('contacts');
+    
+    const request = store.put({ id: id, name: name, email: email, phone: phone, profile: profile });
+    const result = await request;
+    console.log('🚀 - data saved to the database', result);
+  };
+  
   
